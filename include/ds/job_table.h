6e8ecbe3a40396d0c621ad43f3a6d0ae8d259d2c
Leblond--Olive Louis <louis.leblond-olive@student-cs.fr>
2026-07-02 13:10:13 +0200

    adding job table data structure

---
+++ include/ds/job_table.h
@@ -0,0 +1,84 @@
+#ifndef JOB_TABLE
+#define JOB_TABLE
+
+#include <stdlib.h>
+
+/**
+ * @file job_table.h
+ * @brief Job and job table operations.
+ * 
+ * Define the job and job table type, and provide functions to build and 
+ * manipulate job tables in the executor. 
+ */
+
+
+//  ----- JOB MANAGEMENT -----------------------------------------------
+/**
+ * @brief Status of a job execution, determining whether it is running or not. 
+ */
+typedef enum job_status {
+    RUNNING,
+    SUSPENDED
+} job_status_e;
+
+/**
+ * @brief Type of a job. 
+ */
+typedef struct job_s {
+    int job_id;             /** job id provided by the shell to the user */
+    pid_t pg_id;            /** job group pid */
+    job_status_e status;    /** job execution status */
+    char *job_cmd;          /** job underlying command */
+} job_t; 
+
+/**
+ * @brief Creates a new job
+ * @warning The created job will have it's job id set to -1. Job id assignation occurs when the job is 
+ *  added to a job table.
+ * @return A pointer to the new job, or NULL on error. 
+ */
+job_t *job_init(pid_t pg_id, char *job_cmd);    // warning : job id is set when the job is added to the job table 
+
+/**
+ * @brief Frees the job referenced by the given pointer. 
+ */
+void free_job(job_t *job);
+
+
+//  ----- JOB MANAGEMENT -----------------------------------------------
+/**
+ * @brief Type of a job table.
+ * @note The job table is implemented as a dynamic table. The capacity field usage is only relevant 
+ *  for internal table operations (ex. : adding an element).
+ */
+typedef struct job_table_s {
+    size_t capacity;   /** Table current maximal size */
+    job_t **tbl;       /** Job table */
+} job_table_t;
+
+/**
+ * @brief Creates a fresh job table. 
+ * @return Returns a pointer to the table, or NULL on error. 
+ */
+job_table_t *job_table_init(void);
+
+/**
+ * @brief Frees the job table referenced by the given pointer. 
+ * @note All the jobs contained in the table will also be freed. 
+ */
+void free_job_table(job_table_t *job_tbl);
+
+/**
+ * @brief Adds a job to a job table by filling the first gap found, or expaning the table size. 
+ * @return Returns the job id of the added job, i.e. its position in the table. On error, returns -1. 
+ * @note The "job_id" field of the added job is filled automatically by this function. 
+ */
+int job_table_add(job_table_t *job_tbl, job_t *job);
+
+/**
+ * @brief Removes a job from the job table. 
+ */
+void job_table_rm(job_table_t *job_tbl, int job_id);
+
+
+#endif
---
+++ src/ds/job_table.c
@@ -0,0 +1,128 @@
+#include "job_table.h"
+
+
+//  ----- JOB MANAGEMENT -----------------------------------------------
+
+job_t *job_init(pid_t pg_id, char *job_cmd){
+
+    job_t *res = (job_t*)malloc(sizeof(job_t));
+    if(!res) return NULL;
+
+    res->job_id = -1;
+    res->pg_id = pg_id;
+    res->job_cmd = job_cmd;
+    res->status = RUNNING;
+
+    return res;
+}
+
+
+void free_job(job_t *job){
+    if(!job) return;
+
+    free(job->job_cmd);
+    free(job);
+}
+
+
+//  ----- JOB TABLE OPERATIONS -----------------------------------------------
+
+job_table_t *job_table_init(void){
+
+    job_table_t *res = (job_table_t*)malloc(sizeof(job_table_t));
+    if(!res) return NULL;
+
+    res->capacity = 0;
+    res->tbl = NULL;
+
+    return res;
+}
+
+
+void free_job_table(job_table_t *job_tbl){
+
+    if(!job_tbl) return;
+    if(job_tbl->tbl){
+        for (size_t i = 0; i < job_tbl->capacity; i++) free_job(job_tbl->tbl[i]);
+        free(job_tbl->tbl);
+    }
+    free(job_tbl);
+}
+
+
+int job_table_add(job_table_t *job_tbl, job_t *job){
+    if(!job_tbl || !job) return -1;
+
+    if(!job_tbl->tbl){
+        
+        job_tbl->tbl = (job_t**)calloc(2, sizeof(job_t*));
+        if(!job_tbl->tbl) return -1;
+        
+        job_tbl->tbl[0] = NULL;
+        job_tbl->tbl[1] = job;
+        job_tbl->capacity = 2;
+
+        job->job_id = 1;
+        return 1;
+    }
+
+    else {
+
+        size_t gap = 1;
+        while(gap < job_tbl->capacity && job_tbl->tbl[gap] != NULL) gap ++;
+
+        if(gap >= job_tbl->capacity){   // table is full
+
+            job_t **new_tbl = (job_t**)calloc(2 * job_tbl->capacity, sizeof(job_t*));
+            if(!new_tbl) return -1;
+
+            for (size_t i = 1; i < job_tbl->capacity; i++) new_tbl[i] = job_tbl->tbl[i];
+            new_tbl[job_tbl->capacity] = job;
+
+            free(job_tbl->tbl);
+            job_tbl->tbl = new_tbl;
+
+            job->job_id = job_tbl->capacity;
+
+            job_tbl->capacity *= 2;
+            return job->job_id;
+        } 
+
+        else {  // an empty slot is available
+            job_tbl->tbl[gap] = job;
+            job->job_id = gap ;
+
+            return gap;
+        }
+    }
+
+    return -1; // unreachable
+}
+
+
+void job_table_rm(job_table_t *job_tbl, int job_id){
+    if(!job_tbl || !job_tbl->tbl ||
+         job_id <= 0 || job_id >= job_tbl->capacity) return;
+
+    free_job(job_tbl->tbl[job_id]);
+    job_tbl->tbl[job_id] = NULL;
+
+    if(job_tbl->capacity > 2){
+
+        for (size_t i = job_tbl->capacity / 2; i < job_tbl->capacity; i++){
+            if(job_tbl->tbl[i] != NULL) return;
+        }
+        
+        job_t **new_tbl = (job_t**)calloc(job_tbl->capacity / 2, sizeof(job_t*));
+        if(!new_tbl) return;
+
+        for(size_t i = 0; i < job_tbl->capacity / 2; i++) new_tbl[i] = job_tbl->tbl[i];
+
+        free(job_tbl->tbl);
+        job_tbl->tbl = new_tbl;
+
+        job_tbl->capacity /= 2;
+    }
+
+    return;
+}
