baa3fb3f7b7aebf33620fb3e368880fae2b46933
Leblond--Olive Louis <louis.leblond-olive@student-cs.fr>
2026-07-17 19:46:48 +0200

    Format: Completing AST header comments

--- include/ds/ast.h
+++ include/ds/ast.h
@@ -21,16 +21,16 @@
  */
 typedef struct redir_s {
     token_e        type;      /** Redirection type (i.e. '<', '>', '>>'). */
-    segment_t      *target;   /** Redirection target, segmented into its litteral and variable parts. */
+    segment_t      *target;   /** Redirection target, segmented into its literal and variable parts. */
     struct redir_s *next;     /** Next redirection node in the chained list. */
-} redir_t ;
+} redir_t;
 
 /**
  * @brief Type of a redirection chained list.
  */
-typedef struct redir_chain_s{
-    redir_t *first;
-    redir_t *last;
+typedef struct redir_chain_s {
+    redir_t *first;     /** First node of the chain. */
+    redir_t *last;      /** Last node of the chain. */
 } redir_chain_t;
 
 
@@ -39,16 +39,16 @@ typedef struct redir_chain_s{
  * @brief Type of an argument node. 
  */
 typedef struct arg_s {
-    segment_t    *seg_chain;    /** The argument value, segmented into its litteral and variable parts. */
+    segment_t    *seg_chain;    /** The argument value, segmented into its literal and variable parts. */
     struct arg_s *next;         /** Next argument node in the chained list. */
 } arg_t;
 
 /**
  * @brief Type of an argument chained list. 
  */
-typedef struct argv_s{
-    arg_t *first;
-    arg_t *last;
+typedef struct argv_s {
+    arg_t *first;       /** First node of the chain. */
+    arg_t *last;        /** Last node of the chain. */
 } argv_t;
 
 
@@ -65,10 +65,10 @@ typedef struct ast_node_s {
                                   * If set to TOKEN_WORD, the node is to be interpreted as a command node. 
                                   * Otherwise, the node is an operator and the following parameters are to be ignored. 
                                   */
-    argv_t *argv;               /** The argument chained-list of the node. */
-    redir_chain_t *redirs;      /** The redirection chained-list of the node. */
+    argv_t *argv;               /** The argument chained list of the node. */
+    redir_chain_t *redirs;      /** The redirection chained list of the node. */
     
-    char *str_cmd;              /** The text command represented by the ast  */ 
+    char *str_cmd;              /** The text command represented by the AST. */ 
 
 } ast_node_t;
 
@@ -109,12 +109,12 @@ void free_argv(argv_t *argv);
 
 /**
  * @brief Adds an empty argument node to an argument chain.
- * @return Returns 0 on sucess, -1 otherwise. 
+ * @return Returns 0 on success, -1 otherwise. 
  */
 int add_arg(argv_t *argv);
 
 /**
- * @brief Frees a string array formated argument list.
+ * @brief Frees a string array formatted argument list.
  * @note Strings representing arguments value are also freed by this function.  
  */
 void free_arg_array(char **arg_arr);
@@ -135,11 +135,35 @@ int count_args(argv_t *argv);
 char **arg_chain_to_array(env_t *env, argv_t *argv);
 
 // ----- AST OPERATIONS ---------------------------------------------------------------------
-
+/**
+ * @brief Allocate a fresh AST node. 
+ * @return Returns an empty AST node, or NULL on error. 
+ */
 ast_node_t *init_node(void);
+
+/**
+ * @brief Frees an AST recursively from the root. 
+ * @note Each node argument, str_cmd and redirection fields will also be freed. 
+ */
 void free_ast(ast_node_t *root);
+
+/**
+ * @brief Adds a left child node to a parent node. 
+ * @return 0 on success, -1 otherwise. 
+ * @note If a left child is already bound to the node, it will be freed. 
+ */
 int add_child_left(ast_node_t *parent, ast_node_t *child);
+
+/**
+ * @brief Adds a right child node to a parent node. 
+ * @return 0 on success, -1 otherwise. 
+ * @note If a right child is already bound to the node, it will be freed. 
+ */
 int add_child_right(ast_node_t *parent, ast_node_t *child);
+
+/**
+ * @brief Recursively assign all nodes `str_cmd` AST string representation field.  
+ */
 int build_ast_text_cmd(ast_node_t *root);
 
 
