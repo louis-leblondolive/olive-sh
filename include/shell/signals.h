0b1e69164d4f402ff3c51cceee8304ffaf7b08b2
Leblond--Olive Louis <louis.leblond-olive@student-cs.fr>
2026-07-18 19:55:52 +0200

    Format: Adding signal header documentation

--- include/shell/signals.h
+++ include/shell/signals.h
@@ -9,12 +9,43 @@
 
 #include "job_ctl.h"
 
+/**
+ * @file signals.h
+ * @brief Signal handlers and management implementation. 
+ */
+
+/**
+ * @brief Global jump buffer to store program state in case of interruption.
+ */
 extern sigjmp_buf jump_buffer;
+
+/**
+ * @brief Global variable indicating if jump operation is to be used.  
+ */
 extern volatile sig_atomic_t jump_active;  
 
+
+/**
+ * @brief Initializes the signal handlers used by the shell. 
+ * @return 0 on success, 1 otherwise. 
+ */
 int init_shell_sa_handlers(void);
+
+/**
+ * @brief Resets all signal handlers affected by the shell initialization.
+ * @return 0 on success, 1 otherwise.  
+ */
 int reset_sa_handlers(void);
+
+/**
+ * @brief Masks SIGCHLD from being delivered to the process.
+ * @return The process signal mask as it was before SIGCHLD was blocked. 
+ */
 sigset_t block_sigchld(void);
+
+/**
+ * @brief Restores a signal old_mask. 
+ */
 void restore_sigmask(sigset_t old_mask);
 
 #endif
